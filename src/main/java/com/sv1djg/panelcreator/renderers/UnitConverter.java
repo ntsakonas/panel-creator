cfd6a140331c3f2de926a6b29e9d620e6be7b79d
ntsakonas <magicnickus@yahoo.com>
2021-01-16 02:46:49 +0000

    added license on all files

--- src/main/java/com/sv1djg/panelcreator/renderers/UnitConverter.java
+++ src/main/java/com/sv1djg/panelcreator/renderers/UnitConverter.java
@@ -1,3 +1,21 @@
+/*
+    PanelCreator - A program to quickly generate front panels and drilling templates for your DIY projects.
+    Copyright (C) 2019-2021, Nick Tsakonas (SV1DJG)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.sv1djg.panelcreator.renderers;
 
 public class UnitConverter {
